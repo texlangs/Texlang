b44aad5f9d9417121bfdeac3c2145bd1f2b38658
texlang <vijay@intechnovation.com>
2015-11-21 18:31:48 +0530

    new changes

--- Texlang/Controllers/HomeController.cs
+++ Texlang/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Texlang.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your Last application description page.";
 
             return View();
         }
